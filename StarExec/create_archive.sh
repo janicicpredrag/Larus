7395cf0f06a6e8188dd0e63cc19197a4df454918
Julien Narboux <Julien@narboux.fr>
2020-03-17 05:33:41 -0500

    add script

---
+++ StarExec/create_archive.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Fetching the sources..."
+cd ..
+git pull
+echo "Building the prover..."
+make
+echo "Copying CLprover in the bin directory..."
+cd StarExec
+cp ../CLprover bin/
+echo "Creating the archive..."
+tar cvfz CLprover.tgz bin/*
