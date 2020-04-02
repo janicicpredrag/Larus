d3da99be7b32881c69c5db0889737f919c2a8d78
Julien Narboux <narboux@unistra.fr>
2020-04-02 14:06:51 +0200

    suppress warning

--- ProvingEngine/EQ_Engine/EQ_ProvingEngine.cpp
+++ ProvingEngine/EQ_Engine/EQ_ProvingEngine.cpp
@@ -1089,7 +1089,7 @@ bool EQ_ProvingEngine::DecodeSubproof(const DNFFormula& formula, const vector<st
     dummy.SetName("dummy");
 
     CaseSplit* pcs;
-    ByNegIntro* pni;
+    ByNegIntro* pni=nullptr;
     string str;
     if (ursaproof.good())
     {
