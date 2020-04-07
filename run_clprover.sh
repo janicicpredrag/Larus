8a1200953bfffe722ade5b52542fb0807ad74c32
Julien Narboux <narboux@unistra.fr>
2020-04-07 17:09:09 +0200

    add questions and benches

--- run_clprover.sh
+++ run_clprover.sh
@@ -7,12 +7,13 @@ printout() {
 i=0
 time=10
 maxProofLen=32
+startinglength=4
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 filename="clprover-results-$today.out"
 summary="clprover-summary-$today.out"
 
 PS3='Please enter your choice of benches: '
-options=("Euclid" "Tarski" "Coherent logic benches" "Col trans 10" "Col trans 100" "Col trans 1000")
+options=("Euclid" "Tarski" "Coherent logic benches" "Col trans 10" "Col trans 100" "Col trans 1000" "Col trans 10 (Alternate Axioms)" "Col trans 100 (Alternate Axioms)" "Col trans 1000 (Alternate Axioms)" )
 select opt in "${options[@]}"
 do
     case $opt in
@@ -28,24 +29,46 @@ do
             ;;
         "Coherent logic benches")
             echo "$opt selected"
-	    benches="tptp-problems/coherent-logic-benches/*.p"
-	    break
+    	    benches="tptp-problems/coherent-logic-benches/*.p"
+	        break
             ;;
         "Col trans 10")
             echo "$opt selected"
-	    benches="tptp-problems/col-trans/col-trans-000*.p"
-	    break
+            cp tptp-problems/col-trans/col-axioms-orig.ax tptp-problems/col-trans/col-axioms.ax
+	        benches="tptp-problems/col-trans/col-trans-000*.p"
+	        break
             ;;
         "Col trans 100")
-	    echo "$opt selected"
-	    benches="tptp-problems/col-trans/col-trans-00*.p"
+	        echo "$opt selected"
+            cp tptp-problems/col-trans/col-axioms-orig.ax tptp-problems/col-trans/col-axioms.ax
+	        benches="tptp-problems/col-trans/col-trans-00*.p"
             break
             ;;
    	    "Col trans 1000")
-	    echo "$opt selected"
-	    benches="tptp-problems/col-trans/col-trans-0*.p"
-	    break
-	    ;;
+	        echo "$opt selected"
+            cp tptp-problems/col-trans/col-axioms-orig.ax tptp-problems/col-trans/col-axioms.ax
+	        benches="tptp-problems/col-trans/col-trans-0*.p"
+	        break
+	        ;;
+        "Col trans 10 (Alternate Axioms)")
+            echo "$opt selected"
+            cp tptp-problems/col-trans/col-axioms-2.ax tptp-problems/col-trans/col-axioms.ax
+	        benches="tptp-problems/col-trans/col-trans-000*.p"
+	        break
+            ;;
+        "Col trans 100 (Alternate Axioms)")
+	        echo "$opt selected"
+            cp tptp-problems/col-trans/col-axioms-2.ax tptp-problems/col-trans/col-axioms.ax
+	        benches="tptp-problems/col-trans/col-trans-00*.p"
+            break
+            ;;
+   	    "Col trans 1000 (Alternate Axioms)")
+	        echo "$opt selected"
+            cp tptp-problems/col-trans/col-axioms-2.ax tptp-problems/col-trans/col-axioms.ax
+	        benches="tptp-problems/col-trans/col-trans-0*.p"
+	        break
+	        ;;
+
         *) echo "invalid option $REPLY";;
     esac
 done
@@ -79,6 +102,7 @@ do
         *) echo "invalid option $REPLY";;
     esac
 done
+
 
 PS3='Please select axioms: '
 options3=("None" "All" "Excluded Middle Only" "Eq Axioms Only" "Neg Elim Only")
@@ -134,7 +158,6 @@ else
     time=$timev
 fi
 
-
 echo "Max proof length ? (Default is $maxProofLen)"
 read maxProofLen
 if [ -z "$maxProofLen" ]
@@ -165,16 +188,38 @@ do
     esac
 done
 
+echo "Proof nest ? "
+read nestv
+if [ -z "$nestv" ]
+then
+   echo "Using default nesting value."
+else
+    echo "Setting nesting limit"
+    nest=$nestv
+fi
+
+echo "Starting proof size ? "
+read startsizev
+if [ -z "$startsizev" ]
+then
+   echo "Using default starting proof size value."
+else
+    echo "Setting starting proof size"
+    startinglength=$startsizev
+fi
+
 echo "Running clprover with engine: " $engine | tee -a $filename
+echo "Nesting:" $nesting | tee -a $filename
 echo "Time limit: " $time | tee -a $filename
+echo "Starting proof length" $startinglength | tee -a $filename
 echo "Max proof length: " $maxProofLen | tee -a $filename
 echo "Find shortest proof:" $opt4 | tee -a $filename
 
 for file in $benches
 do
   echo No: $i; echo "Trying file $file ..." | tee -a $filename
-  echo -l"$time" $engine -ftptp -vcoq -p"$maxProofLen" -s$minproof -n1 -vcoq "$axioms" "$axiomsb" "$file"
-  ./CLprover -l"$time" -p"$maxProofLen" $minproof $engine -n1 -ftptp -vcoq "$eqaxioms" "$neaxioms" "$exaxioms" "$file" | tee -a $filename
+  echo -l"$time" $engine -ftptp -vcoq -p"$maxProofLen" $minproof -vcoq "$axioms" "$axiomsb" "$file"
+  ./CLprover -l"$time" -m$startinglength -p"$maxProofLen" -n"$nest" $minproof $engine -ftptp -vcoq "$eqaxioms" "$neaxioms" "$exaxioms" "$file" | tee -a $filename
   ((i++))
   echo "Number of theorems proved until now:" | tee -a $summary
   grep Theorem < $filename | wc -l | tee -a $summary
@@ -182,7 +227,11 @@ done
 echo "------------------------------------------------------"
 echo "Summary:"
 echo "Benches:" $opt | tee -a $summary
-echo "Time given: $time" | tee -a $summary
+echo "Time limit: $time" | tee -a $summary
+echo "Starting proof length: " $startinglength | tee -a $filename
+echo "Max proof length: " $maxProofLen | tee -a $filename
+echo "Find shortest proof:" $opt4 | tee -a $filename
+echo "Nesting:" $nest | tee -a $filename
 echo "Engine: $opt2" | tee -a $summary
 echo "Number of benches" $i | tee -a $summary
 echo "Number of theorems proved:" | tee -a $summary
