6d6622f4024573b2e21d86c9221cd364dfb24050
Julien Narboux <narboux@unistra.fr>
2020-04-08 15:20:31 +0200

    recude proof search of integration testing

--- run_ci.sh
+++ run_ci.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
+maxlength=-p16
 benches=tptp-problems/continuous-integration/*.p
 for file in $benches
 do
     echo No: $i; echo "Trying file $file ..."
     echo "With URSA:"
-    ./CLprover -eursa -ftptp -vcoq "$file"
+    ./CLprover $maxlength -eursa -ftptp -vcoq "$file"
     echo "With STL:"
-    ./CLprover -estl -ftptp -vcoq "$file"
+    ./CLprover $maxlength -estl -ftptp -vcoq "$file"
     echo "With SMTBV:"
-    ./CLprover -esmtbv -ftptp -vcoq "$file"
+    ./CLprover $maxlength -esmtbv -ftptp -vcoq "$file"
     echo "With SMTLIA:"
-    ./CLprover -esmtlia -ftptp -vcoq "$file"
+    ./CLprover $maxlength -esmtlia -ftptp -vcoq "$file"
     ((i++))
 done
