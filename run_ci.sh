ff521332a9775a58f86c858cec02f960851bc279
Julien Narboux <narboux@unistra.fr>
2020-04-09 15:39:34 +0200

    improve continuous integration script

--- run_ci.sh
+++ run_ci.sh
@@ -3,16 +3,39 @@ maxlength=-p32
 axioms1=-aexcludedmiddle 
 axioms2=-anegelim
 benches=tptp-problems/continuous-integration/*.p
+today=`date '+%Y_%m_%d__%H_%M_%S'`;
+filename="results/bugs-$today.out"
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+test_success () {
+    if grep Theorem $1 > /dev/null; then
+        if grep Correct $1 > /dev/null; then
+         echo "${GREEN} Coq Ok ${NC}"
+        else 
+         echo "${ORANGE} Ok, but Coq fails ${NC}"
+        fi
+    else
+        echo "${RED} Error ${NC}"
+        cat < $1 >> $filename
+    fi
+}
 for file in $benches
 do
     echo No: $i; echo "Trying file $file ..."
-    echo "With URSA:"
-    ./CLprover -l30 $maxlength $axioms $axioms2 -n3 -eursa -ftptp -vcoq "$file"
-    echo "With STL:"
-    ./CLprover -l30 $maxlength $axioms $axioms2 -n3 -estl -ftptp -vcoq "$file"
-    echo "With SMTBV:"
-    ./CLprover -l30 $maxlength $axioms $axioms2 -n3 -esmtbv -ftptp -vcoq "$file"
-    echo "With SMTLIA:"
-    ./CLprover -l30 $maxlength $axioms $axioms2 -n3 -esmtlia -ftptp -vcoq "$file"
+    printf "URSA:  "
+    ./CLprover -l30 $maxlength $axioms $axioms2 -n3 -eursa -ftptp -vcoq "$file" > resursa.txt
+    test_success resursa.txt
+    printf "STL:   "
+    ./CLprover -l30 $maxlength $axioms $axioms2 -n3 -estl -ftptp -vcoq "$file" > resstl.txt
+    test_success resstl.txt
+    printf "SMTBV: "
+    ./CLprover -l30 $maxlength $axioms $axioms2 -n3 -esmtbv -ftptp -vcoq "$file" > ressmtbv.txt
+    test_success ressmtbv.txt
+    printf "SMTLIA:"
+    ./CLprover -l30 $maxlength $axioms $axioms2 -n3 -esmtlia -ftptp -vcoq "$file" > ressmtlia.txt
+    test_success ressmtlia.txt
     ((i++))
 done
+echo "Output of errors have been written to $filename"
