792586f0d670871ae90768ddcae48f4af2bc47c7
janicic <janicic@matf.bg.ac.rs>
2020-06-30 11:59:36 +0200

    using temp files

--- ProvingEngine/EQ_Engine/EQ_ProvingEngine.cpp
+++ ProvingEngine/EQ_Engine/EQ_ProvingEngine.cpp
@@ -370,7 +370,7 @@ bool EQ_ProvingEngine::ProveFromPremises(const DNFFormula& formula, CLProof& pro
                 break;
 
             DECLARATIONS = decl;
-            string smt_proofencoded_filename = "prove.smt"; // tmpnam(NULL); //
+            string smt_proofencoded_filename =  tmpnam(NULL); //"prove.smt"; //
             string smt_model_filename =  tmpnam(NULL); //"smt-model.txt"; //
             
             EncodeProof(formula, l, smt_proofencoded_filename);
