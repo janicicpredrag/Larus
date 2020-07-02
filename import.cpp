da25f01599a7f6102036e247d50964f3797fd091
janicic <janicic@matf.bg.ac.rs>
2020-07-02 21:15:32 +0200

    a check before adding a constant if there is no one

--- import.cpp
+++ import.cpp
@@ -79,7 +79,10 @@ bool stoi(string s, int& i)
 
 ReturnValue ProveTheorem(Theory& T, ProvingEngine* engine, CLFormula& theorem, const string& theoremName, const string& theoremFileName, proverParams& params, const vector<tHint>& hints)
 {
-    T.MakeNewConstant();
+    // fixme: this check should be activated?
+    if (T.mConstants.size() + T.mConstantsPermissible.size() == 0)
+        T.MakeNewConstant();
+
     T.StoreInitialConstants();
 
     map<string,string> instantiation;
