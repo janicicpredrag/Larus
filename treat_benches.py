ac8de608bff2f69613142be806e476d38f1847c5
Julien Narboux <narboux@unistra.fr>
2020-10-06 11:19:45 +0200

    improve figures

--- treat_benches.py
+++ treat_benches.py
@@ -17,11 +17,11 @@ def number_of_benches(data, list_of_provers, bench_name):
 def generate_graph(filename, list_of_provers_colors, bench_directory, bench_display_name, maxtime):
     for (prover_name,color_name) in list_of_provers_colors:
         plt.plot(range(1,maxtime), number_solved_in_less_than(data, prover_name, bench_directory, maxtime), color=color_name, label = prover_name)
-    plt.ylim(1,number_of_benches(data, map(lambda x:x[0],list_of_provers_colors), bench_directory)+5) 
+    plt.ylim(1,number_of_benches(data, map(lambda x:x[0],list_of_provers_colors), bench_directory)+10) 
     plt.xlim(1,maxtime) 
-    plt.xlabel('time') 
+    plt.xlabel('time in seconds') 
     plt.ylabel('number of problem solved') 
-    plt.title('Results on ' + bench_display_name) 
+    plt.title('Results about ' + bench_display_name) 
     plt.legend() 
     plt.savefig(filename)
     plt.show()    
@@ -29,6 +29,8 @@ def generate_graph(filename, list_of_provers_colors, bench_directory, bench_disp
 with open('data2.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
     data= list( row for row in reader)
-    generate_graph("col-trans-graph.pdf", [("CLprover","red"),("vampire","green"),("eprover","darkred"),("zenon","darkblue"),("nanocop","orange"),("leancop","pink"),("ChewTPTP","purple")], "col-trans", "Col transitivity", 100)
-    generate_graph("euclid-graph.pdf", [("vampire","green"),("eprover","red")], "euclid", "Euclid", 100)
-    
+    big_list= [("CLprover","red"),("vampire","green"),("eprover","darkred"),("zenon","darkblue"),("nanocop","orange"),("leancop","pink"),("ChewTPTP","purple")]
+    generate_graph("col-trans-graph.pdf", big_list, "col-trans", "Col transitivity", 100)
+    generate_graph("euclid-graph.pdf", big_list, "euclid", "Euclid Book I", 100)
+    generate_graph("cl-benches-graph.pdf", big_list, "coherent", "Coherent Logic Benches", 100)
+  
