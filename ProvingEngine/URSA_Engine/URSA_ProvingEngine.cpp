0ba877392e56f63d5506cd6006c9cdfb90801f1d
Predrag Janicic <janicic@matf.bg.ac.rs>
2021-10-13 21:50:56 +0200

    fix in using MAX_NESTING constant in URSA encoding

--- ProvingEngine/URSA_Engine/URSA_ProvingEngine.cpp
+++ ProvingEngine/URSA_Engine/URSA_ProvingEngine.cpp
@@ -858,7 +858,7 @@ void URSA_ProvingEngine::EncodeProof(const DNFFormula &formula) {
     ursaFile << "                    && bCases[nProofStep-1] " << endl;
     ursaFile << "                    && !bCases[nProofStep] " << endl;
     // harmonize possible values for nesting TODO
-    ursaFile << "                    && (nNesting[nProofStep-1] < MAX_NESTING) "
+    ursaFile << "                    && (nNesting[nProofStep-1] < " << MAX_NESTING << ") "
              << endl;
     ursaFile << "                    && (nNesting[nProofStep] == "
                 "(nNesting[nProofStep-1]<<1)) "
